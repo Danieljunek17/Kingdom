6c0d0ff1cd715896753662f77bc8da7da15f053b
Danieljunek17 <cedric@denbaes.org>
2021-12-08 00:58:02 +0100

    general menu abstract class upgrade

--- src/main/java/nl/republicmc/kingdom/feature/Menu.java
+++ src/main/java/nl/republicmc/kingdom/feature/Menu.java
@@ -1,12 +1,19 @@
 package nl.republicmc.kingdom.feature;
 
 import de.themoep.inventorygui.InventoryGui;
+import nl.republicmc.kingdom.KingdomPlugin;
 import org.bukkit.entity.Player;
 
 public abstract class Menu {
 
-    public abstract void show(Player player);
+    public KingdomPlugin plugin;
+
+    public InventoryGui gui;
+
+    public Menu(KingdomPlugin plugin) {
+    }
 
     public abstract InventoryGui getGui();
 
+    public abstract void show(Player player);
 }
